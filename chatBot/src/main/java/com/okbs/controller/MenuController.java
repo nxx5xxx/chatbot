1b4e54ae15c7abb268e9a858b2878868f35accbf
nxx5xxx <dnwnx123@naver.com>
2023-10-05 15:22:40 +0900

    bot , human interface and implement
    중간저장 기능자체는 구현을 해놨지만 기능을 더 추가해야할것같다

--- chatBot/src/main/java/com/okbs/controller/MenuController.java
+++ chatBot/src/main/java/com/okbs/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.okbs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.okbs.service.MenuService;
@@ -12,9 +13,22 @@ public class MenuController {
 	@Autowired
 	MenuService menuService;
 	
+//	@GetMapping("/send.do")
+//	@ResponseBody
+//	public String send() {
+//		return "데이터값";
+//	}
+	
 	@GetMapping("/send.do")
 	@ResponseBody
-	public String send() {
-		return "데이터값";
+	public String send(@RequestParam("title") String title) {
+		String data = "";
+		try {
+			data = menuService.selectMenu(title).getDesc();
+		} catch (Exception e) {
+			data = "해당하는 내역이 없습니다. 도움말을 입력해보세요!";
+		}
+		
+		return data;
 	}
 }
